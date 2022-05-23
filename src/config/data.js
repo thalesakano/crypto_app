02e3f01a0ac7d38e6e9c2fd324a9b8e26e236c1f
thalesakano <54159920+thalesakano@users.noreply.github.com>
2022-05-23 07:32:24 -0300

    initial deploy

---
+++ src/config/data.js
@@ -0,0 +1,18 @@
+export const chartDays = [
+  {
+    label: "24 Horas",
+    value: 1,
+  },
+  {
+    label: "30 Dias",
+    value: 30,
+  },
+  {
+    label: "3 Meses",
+    value: 90,
+  },
+  {
+    label: "1 Ano",
+    value: 365,
+  },
+];
